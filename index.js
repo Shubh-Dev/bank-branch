1fecd756992eda0aab59f8a09cf3b18817bbc7ed
Shubh <shubh.scb@gmail.com>
2023-02-07 21:47:55 +0530

    LINTERS ERR FIX

--- index.js
+++ index.js
@@ -5,32 +5,30 @@ const app = express();
 const port = 3000;
 
 const banks = [
-    {
-        id: 1,
-        name: 'Axis'
-    },
+  {
+    id: 1,
+    name: 'Axis',
+  },
 
-    {
-        id: 2,
-        name: 'Icici' 
-    },
+  {
+    id: 2,
+    name: 'Icici',
+  },
 
-
-    {
-        id: 3,
-        name: 'HDFC' 
-    }
+  {
+    id: 3,
+    name: 'HDFC',
+  },
 ];
 
 // get the bank list in json format
 
 app.get('/banks', (req, res) => {
-    res.json(banks);
-
+  res.json(banks);
 });
 
 // set the server to listen to the port
 
 app.listen(port, () => {
-    console.log(`Listening at localhost ${port}`)
+  console.log(`Listening at localhost ${port}`);
 });
