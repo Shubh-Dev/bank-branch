12a3087ddb2c7e71765913ef50036dcd68a30a11
Shubh <shubh.scb@gmail.com>
2023-02-07 20:36:12 +0530

    BANK OBJECT CREATE

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+const banks = [
+    {
+        id: 1,
+        name: 'Axis'
+    },
+
+    {
+        id: 2,
+        name: 'Icici' 
+    },
+
+
+    {
+        id: 3,
+        name: 'HDFC' 
+    }
+];
+
+// get the bank list in json format
+
+app.get('/banks', (req, res) => {
+    req.json(banks);
+
+});
+
+// set the server to listen to the port
+
+app.listen(port);
