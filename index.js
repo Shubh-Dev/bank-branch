982a6c6fb7686a4cb5e3992e8a0dfc1acfe97d2b
Shubh <shubh.scb@gmail.com>
2023-02-08 21:21:28 +0530

    POST METHOD ADD

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+const { urlencoded } = require('express');
 const express = require('express');
 
 const app = express();
 
 const port = 3000;
+
+// parse json using express
+
+app.use(express.json());
+app.use(urlencoded({extended: false}))
 
 const banks = [
   {
@@ -27,6 +33,18 @@ app.get('/banks', (req, res) => {
   res.json(banks);
 });
 
+// add banks to the list
+
+app.post('/banks', (req, res) => {
+  const bank =  req.body;
+  console.log(bank);
+  banks.push(bank);
+
+  res.send('Bank is added to the list')
+});
+
 // set the server to listen to the port
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Server listening at port ${port}`)
+});
